55f927a695ea52feb42f75d1175e3a1493a3641f
Luuk <34650343+luukvandeven@users.noreply.github.com>
2021-01-29 10:38:08 +0100

    Update Header.vue

--- docs/.vuepress/theme/global-components/Header.vue
+++ docs/.vuepress/theme/global-components/Header.vue
@@ -31,7 +31,7 @@ header .line {
   width: 50%;
   border-bottom-width: 1px;
   border-bottom-style: solid;
-  border-bottom-color: black;
+  border-bottom-color: rgba(0, 0, 0, 0.33);
   height: 1px;
 }
 
