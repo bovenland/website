ccf4f95f61440395d751b62d6ed2dca9643f2fc8
Harm van de Ven <harm@harmvandeven.nl>
2021-09-28 09:53:54 +0200

    Improve styling of the FoldDownMenu

--- components/menu/Burger.vue
+++ components/menu/Burger.vue
@@ -30,7 +30,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import "@/assets/sass/partials/base.scss";
 @import "@/assets/sass/partials/mixins.scss";
 
--- components/menu/FoldDownMenu.vue
+++ components/menu/FoldDownMenu.vue
@@ -5,12 +5,17 @@
       :key="index"
       v-bind:class="'folddown-' + child.foldDown"
     >
-      <FoldDown v-bind:active="child.foldDown">
-        <div style="cursor: pointer" v-on:click="foldDown(child)">
-          <h4>{{ child.index }}</h4>
+      <div class="fold-down-menu-button" v-on:click="foldDown(child)">
+        <FoldDown v-bind:active="child.foldDown">
+          <label>Hoofdstuk {{ child.index }}</label>
           <h2>{{ child.title }}</h2>
-        </div>
-      </FoldDown>
+        </FoldDown>
+      </div>
+
+      <div class="description">
+        {{ child.description }}
+      </div>
+
       <ul>
         <li
           v-for="(segment, segmentIndex) of child.segments"
@@ -31,19 +36,62 @@
 </template>
 
 <style lang="scss" scoped>
+@import "@/assets/sass/partials/base.scss";
+@import "@/assets/sass/partials/mixins.scss";
 ol {
   list-style: none;
 
-  li {
+  > li {
+    h2 {
+      color: $pink;
+      font-size: 20px;
+    }
+    label {
+      color: $label-color;
+    }
+
+    background-color: $white;
+    transition: background-color 0.6s linear;
     ul {
       max-height: 100vh;
       transition: max-height 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
     }
+    div.description {
+      max-height: 100vh;
+      transition: max-height 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+    }
     &:not(.folddown-visible) {
+      background-color: $light-gray;
       ul {
         overflow: hidden;
         max-height: 0px;
       }
+      div.description {
+        overflow: hidden;
+        max-height: 0;
+      }
+    }
+  }
+
+  > li:not(.folddown-visible) + li:not(.folddown-visible) {
+    border-top: 1px solid $border-color;
+  }
+}
+</style>
+
+<style lang="scss">
+@import "@/assets/sass/partials/base.scss";
+@import "@/assets/sass/partials/mixins.scss";
+div.fold-down-menu-button {
+  cursor: pointer;
+  position: relative;
+  padding-right: 30px;
+  svg {
+    position: absolute;
+    right: 13px;
+    @include calc("top", "50% - 12px");
+    &.active {
+      @include opacity(0.25);
     }
   }
 }
