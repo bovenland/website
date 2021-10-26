f6595d10ab89eb7cd9de08fcd77e483c5c242d77
Bert Spaan <bert.spaan@gmail.com>
2021-10-26 11:36:38 +0200

    added mapbox gl to head.

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,36 +9,45 @@ export default {
       lang: 'nl'
     },
     meta: [{
-        charset: 'utf-8'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: ''
-      }
+      charset: 'utf-8'
+    },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1'
+    },
+    {
+      hid: 'description',
+      name: 'description',
+      content: ''
+    }
     ],
     link: [{
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Space+Grotesk:wght@400;600;700&family=Space+Mono:wght@700&display=swap'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://unpkg.com/swiper@7/swiper-bundle.min.css'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://api.mapbox.com/mapbox-gl-js/v2.5.1/mapbox-gl.css'
+    }],
+    script: [
+      {
+        src: 'https://unpkg.com/swiper@7/swiper-bundle.min.js',
+        body: true
       },
       {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Space+Grotesk:wght@400;600;700&family=Space+Mono:wght@700&display=swap'
-      },
-      {
-        rel: 'stylesheet',
-        href: 'https://unpkg.com/swiper@7/swiper-bundle.min.css'
-      },
-    ],
-    script: [{
-      src: 'https://unpkg.com/swiper@7/swiper-bundle.min.js',
-      body: true,
-    }]
+        src: 'https://api.mapbox.com/mapbox-gl-js/v2.5.1/mapbox-gl.js',
+        body: true
+      }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
