350df67cab972814275d3cadfb29dc8c14b83289
Bert Spaan <bert.spaan@gmail.com>
2021-10-26 11:25:56 +0200

    added empty map page for chapter 2

---
+++ pages/hoofdstukken/vijftien-minuten-verderop/cartografie/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <label>Hoofdstuk 2</label>
+    <h1>Vijftien minuten verderop</h1>
+    <p>Cartografie</p>
+    <p>
+      hier de kaart!!!
+
+    </p>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
