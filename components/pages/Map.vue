9a892c18cc39a74c6615fda4bd4e54c4f39507ee
Bert Spaan <bert.spaan@gmail.com>
2021-10-26 09:49:07 +0200

    added map page stub

---
+++ components/pages/Map.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="map-page">
+    <!-- Segmenten menu -->
+    <SegmentsMenu
+      v-bind:chapter="chapter"
+      v-bind:segment="segment"
+      v-bind:segments="segments"
+    />
+
+    <div class="container column chapter">
+      <slot />
+    </div>
+  </div>
+</template>
