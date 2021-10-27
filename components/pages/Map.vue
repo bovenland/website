3f67cdd2f4bfaab2a94e882ddc85b54825650949
Harm van de Ven <harm@harmvandeven.nl>
2021-10-27 11:05:12 +0200

    Remove main "column" class

--- components/pages/Map.vue
+++ components/pages/Map.vue
@@ -12,3 +12,45 @@
     </div>
   </div>
 </template>
+
+<script>
+import structure from "/structure.js";
+import SegmentsMenu from "./../menu/SegmentsMenu";
+
+export default {
+  data() {
+    var chapter = null;
+    var segment = null;
+    structure.chapters.forEach((c) => {
+      if (c.to && chapter == null) {
+        if ("hoofdstukken-" + c.to.name == this.$route.name) {
+          chapter = c;
+        }
+      }
+      if (chapter == null && c.segments) {
+        c.segments.forEach((s) => {
+          if (
+            "hoofdstukken-" + c.to.name + "-" + s.to.name ==
+            this.$route.name
+          ) {
+            chapter = c;
+            segment = s;
+          }
+        });
+      }
+    });
+    if (chapter == null) {
+      return this.$nuxt.error({ statusCode: 404, message: "Page not found" });
+    }
+    return {
+      chapter: chapter,
+      segment: segment,
+      segments: chapter.segments,
+    };
+  },
+
+  components: {
+    SegmentsMenu,
+  },
+};
+</script>
