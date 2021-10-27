5d3c932f6f351addf02e11badf6fb08c82ce602c
Bert Spaan <bert.spaan@gmail.com>
2021-10-27 12:52:23 +0200

    added minimap stub

---
+++ components/elements/MiniMap.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <circle r="50" cx="50" cy="50" />
+  </svg>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+svg {
+  width: 100px;
+}
+</style>
