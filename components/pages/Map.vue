271bb371d7a5063d738abf367b4bb07c1898a914
Bert Spaan <bert.spaan@gmail.com>
2021-10-27 16:26:58 +0200

    added mapbox gl map to mappage

--- components/pages/Map.vue
+++ components/pages/Map.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="map-page">
+    <div ref="map" class="map">
+    </div>
     <!-- Segmenten menu -->
     <SegmentsMenu
       v-bind:chapter="chapter"
       v-bind:segment="segment"
       v-bind:segments="segments"
     />
-
     <div class="container column chapter">
       <slot />
     </div>
@@ -18,7 +19,7 @@ import structure from "/structure.js";
 import SegmentsMenu from "./../menu/SegmentsMenu";
 
 export default {
-  data() {
+  data () {
     var chapter = null;
     var segment = null;
     structure.chapters.forEach((c) => {
@@ -48,9 +49,30 @@ export default {
       segments: chapter.segments,
     };
   },
-
   components: {
     SegmentsMenu,
   },
+  mounted: function () {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYmVydHNwYWFuIiwiYSI6ImR3dERiQk0ifQ.DLbScmbRohc3Sqv7prfhqw'
+
+    const map = new mapboxgl.Map({
+      container: this.$refs.map,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-74.5, 40],
+      zoom: 9
+    })
+  }
 };
 </script>
+
+<style scoped>
+.map {
+  position: absolute;
+  z-index: 0;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: pink;
+}
+</style>
