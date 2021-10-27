4815217e8809a9dc7f1bfee554135937a7efd739
Bert Spaan <bert.spaan@gmail.com>
2021-10-27 17:19:41 +0200

    read center and zoom from route query

--- components/elements/MiniMap.vue
+++ components/elements/MiniMap.vue
@@ -1,7 +1,14 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-    <circle r="50" cx="50" cy="50" />
-  </svg>
+  <NuxtLink :to="{
+    name: 'hoofdstukken-waar-we-winkelen-cartografie',
+    // TODO: use named route instead of path?
+    // path: '/hoofdstukken/waar-we-winkelen/cartografie',
+    query: { center: [4.922, 52.362].join(','), zoom: 12 }
+  }">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+      <circle r="50" cx="50" cy="50" />
+    </svg>
+  </NuxtLink>
 </template>
 
 <script>
