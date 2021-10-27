b84382f7379c4b720bd32322ebe442a965403103
Harm van de Ven <harm@harmvandeven.nl>
2021-10-27 11:05:12 +0200

    Remove main "column" class

--- pages/hoofdstukken/waar-we-winkelen/cartografie/index.vue
+++ pages/hoofdstukken/waar-we-winkelen/cartografie/index.vue
@@ -1,15 +1,33 @@
 <template>
-  <section>
-    <label>Hoofdstuk 1</label>
-    <h1>Waar we winkelen</h1>
-    <p>Cartografie</p>
-    <p>
-      hier de kaart
-
-    </p>
-  </section>
+  <MapPage>
+    <div>
+      <label>Hoofdstuk 1</label>
+      <h1>Waar we winkelen</h1>
+      <p>Cartografie</p>
+      <p>hier de kaart</p>
+    </div>
+  </MapPage>
 </template>
 
 <script>
-export default {};
+import MapPage from "./../../../../components/pages/Map";
+
+export default {
+  transition: "map",
+  components: {
+    MapPage,
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+div {
+  position: absolute;
+  z-index: 0;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: pink;
+}
+</style>
