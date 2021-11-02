06c02158000c977b3b4427b51e3121b4bae2bc1c
Bert Spaan <bert.spaan@gmail.com>
2021-11-02 10:53:20 +0100

    added old map story code from vuepress project

---
+++ components/map/VNode.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'VNode',
+  props: ['node'],
+  render: function () {
+    return this.node ? this.node : ''
+  },
+  mounted: function () {
+    this.$emit('updated')
+  },
+  updated: function () {
+    this.$emit('updated')
+  }
+}
+</script>
