cd8621122b3621f999bf0c0f7381c8bb335cdffc
Harm van de Ven <harm@harmvandeven.nl>
2021-12-03 11:19:38 +0100

    Improve desktop menu transition + styling

--- components/menu/TopMenu.vue
+++ components/menu/TopMenu.vue
@@ -26,12 +26,15 @@ export default {
   overflow-y: auto;
   background-color: $aqua;
   position: fixed;
-  left: 0;
+  right: 0;
   top: 0;
   max-height: 100vh;
   height: 100vh;
   z-index: 999;
-  width: 100vw;
+  width: 400px;
+  @media screen and (max-width: $column-width) {
+    width: 100vw;
+  }
 
   > section {
     padding: 4rem 2rem 2rem 2rem;
@@ -39,14 +42,27 @@ export default {
 
   &.slide-enter-active,
   &.slide-leave-active {
-    transition: max-height 0.3s ease-out 0s top 0.5s ease-in 0s;
+    @media screen and (max-width: $column-width) {
+      transition: max-height 0.3s ease-out 0s top 0.5s ease-in 0s;
+    }
+    @media screen and (min-width: $column-width+1) {
+      transition: right 0.3s ease-out 0s width 0.5s ease-in 0s;
+    }
   }
 
   &.slide-enter,
   &.slide-leave-to {
-    transition: max-height 0.8s ease-out 0s top 1s ease-in 0s;
-    top: -60px;
-    max-height: 0px;
+    @media screen and (max-width: $column-width) {
+      top: -60px;
+      max-height: 0px;
+      transition: max-height 0.3s ease-out 0s top 0.5s ease-in 0s;
+    }
+    @media screen and (min-width: $column-width+1) {
+      right: -60px;
+      width: 0px;
+      transition: right 0.8s ease-out 0s width 1s ease-in 0s;
+    }
+
     overflow: hidden;
   }
 }
