bb61560f7b828c4e170ac9ff41ff5dbf0324b14d
Harm van de Ven <harm@harmvandeven.nl>
2021-12-06 20:21:32 +0100

    Bind the window resize

--- components/pages/Story.vue
+++ components/pages/Story.vue
@@ -295,9 +295,11 @@ export default {
 
   beforeMount() {
     window.addEventListener("scroll", this.handleScroll);
+    window.addEventListener("resize", this.handleScroll);
   },
   beforeDestroy() {
     window.removeEventListener("scroll", this.handleScroll);
+    window.removeEventListener("resize", this.handleScroll);
   },
 
   components: {
