de8c8311f085793848834127e9774c6f99abdc6e
Harm van de Ven <harm@harmvandeven.nl>
2021-12-14 16:16:16 +0100

    Add DataPage

--- pages/hoofdstukken/waar-we-winkelen/data/index.vue
+++ pages/hoofdstukken/waar-we-winkelen/data/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <section>
-    <p>Data</p>
-  </section>
+  <DataPage>
+    <section>
+      <p>Data</p>
+    </section>
+  </DataPage>
 </template>
 
 <script>
-export default {}
+import DataPage from "./../../../../components/pages/Data.vue";
+export default {
+  transition: "story",
+  components: {
+    DataPage,
+  },
+};
 </script>
