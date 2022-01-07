d0619914dc4793fd325deebfd5971a2a30b90092
Bert Spaan <bert.spaan@gmail.com>
2022-01-07 11:17:30 +0100

    first version of data pages

--- pages/hoofdstukken/vijftien-minuten-verderop/data/index.vue
+++ pages/hoofdstukken/vijftien-minuten-verderop/data/index.vue
@@ -1,36 +1,59 @@
 <template>
   <DataPage>
     <section>
-      <p>Data</p>
-<!--
-The 15-Minute City
-https://www.15minutecity.com/
+      <h2>Research</h2>
 
+      <ul>
+        <li>
+          <a href="https://www.15minutecity.com/">
+            The 15-Minute City
+          </a>
+        </li>
 
-MapLab: Measure of connection
-https://www.bloomberg.com/news/newsletters/2021-02-10/maplab-how-car-centric-street-networks-make-buses-less-efficient
+        <li>
+          <a href="https://www.bloomberg.com/news/newsletters/2021-02-10/maplab-how-car-centric-street-networks-make-buses-less-efficient">
+            MapLab: How Car-Centric Street Networks Make Buses Less Efficient
+          </a>
+        </li>
 
-Hundred-mile-long linear city
-https://www.archpaper.com/2021/01/saudi-arabia-reveals-the-line-but-critics-are-skeptical/
+        <li>
+          <a href="https://www.archpaper.com/2021/01/saudi-arabia-reveals-the-line-but-critics-are-skeptical/">
+            Saudi Arabia reveals a hundred-mile-long linear city, but critics are skeptical
+          </a>
+        </li>
 
-TED: Carlos Moreno - The 15 minute city
-https://www.ted.com/talks/carlos_moreno_the_15_minute_city
+        <li>
+          <a href="https://www.ted.com/talks/carlos_moreno_the_15_minute_city">
+            TED: Carlos Moreno - The 15 minute city
+          </a>
+        </li>
 
-Meer bereikbaarheid! Sneller van A naar B! Elke partij wil het, ook al lost het geen enkel probleem op
-https://decorrespondent.nl/12132/meer-bereikbaarheid-sneller-van-a-naar-b-elke-partij-wil-het-ook-al-lost-het-geen-enkel-probleem-op/1275644313900-4c9742e2
+        <li>
+          <a href="https://decorrespondent.nl/12132/meer-bereikbaarheid-sneller-van-a-naar-b-elke-partij-wil-het-ook-al-lost-het-geen-enkel-probleem-op/1275644313900-4c9742e2">
+            Meer bereikbaarheid! Sneller van A naar B! Elke partij wil het, ook al lost het geen enkel probleem op
+          </a>
+        </li>
 
-https://dezwijger.nl/programma/15-minuten-nederland -->
+        <li>
+          <a href="https://dezwijger.nl/programma/15-minuten-nederland">
+            Pakhuis de Zwijger: 15 minuten Nederland
+          </a>
+        </li>
+      </ul>
 
+      <h2>Data</h2>
 
-<!--
-  screenshots
-  zeshoeken
-  osrm
-  procedure
-  github
+      <p>
+        Informatie over de data en broncode die voor dit hoofdstuk gebruikt zijn staat op <a href="https://github.com/bovenland/vijftien-minuten-verderop">GitHub</a>.
+      </p>
 
- -->
-
+      <!--
+        screenshots
+        zeshoeken
+        osrm
+        procedure
+        github
+      -->
 
     </section>
   </DataPage>
--- pages/hoofdstukken/waar-we-winkelen/data/index.vue
+++ pages/hoofdstukken/waar-we-winkelen/data/index.vue
@@ -1,63 +1,227 @@
 <template>
   <DataPage>
     <section>
-      <p>Data</p>
+      <h2>Research</h2>
 
-<!--
+      <h3>Schaalvergroting</h3>
 
+      <ul>
+        <li>College van Rijksadviseurs:
+          <ul>
+            <li>
+              <a href="https://www.collegevanrijksadviseurs.nl/actueel/nieuws/2019/10/29/advies-xxl-verdozing">
+                (X)XL verdozing - Minder, compacter, geconcentreerder, multifunctioneler
+              </a>
+            </li>
+            <li>
+              <a href="https://www.collegevanrijksadviseurs.nl/adviezen-publicaties/publicatie/2020/03/05/verder-met-de-verklaring-van-davos">
+                Verder met de Verklaring van Davos
+              </a>
+            </li>
+            <li>
+              <a href="https://www.collegevanrijksadviseurs.nl/adviezen-publicaties/publicatie/2018/12/06/panorama-nl">
+                Panorama Nederland
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li><a href="https://www.npostart.nl/KN_1711999">De Monitor: Hoe stoppen we de 'verdozing' van Nederland?</a></li>
 
-Schaalvergroting
+        <li>
+          <a href="https://www.vpro.nl/programmas/tegenlicht/kijk/afleveringen/2019-2020/Op-volle-retouren.html">
+            Tegenlicht: Op volle retouren
+          </a>
+        </li>
 
-College van Rijksadviseurs:
-(X)XL verdozing - Minder, compacter, geconcentreerder, multifunctioneler
-Verder met de Verklaring van Davos
-Panorama Nederland
-De Monitor: Hoe stoppen we de 'verdozing' van Nederland?
-op volle retouren Tegenlicht uitzending over retourzendingen van online aankopen.
-Cobouw: Lelijke, grote, grijze doos? Nee hoor, dit wordt een state of the art distributiecentrum, maat XXXL
-2019 recordjaar voor XXL-distributiecentra in Nederland
-Pakketmarkt groeit uit z’n voegen
-Stadsleven: Wat doen we met de lege dozen?
-Vijf weken undercover in het distributiecentrum van bol.com
-Vandaag besteld, morgen in huis: mee op reis met een pakketje van bol.com
-DHG Distripark Maasvlakte West, in samenwerking met de Rotterdamse haven (210.000 m2).
-Zalando bouwt in Bleiswijk een distributiecentrum van 140.000 m2 (20 voetbalvelden).
-Jeroen van Bergeijk: binnen bij bol.com. `undercover bij de winkel van ons allemaal.uitgeverij Fosfor 2019
-Marktstudie last-mile pakketbezorging
-Niemand in Nuenen en Geldrop-Mierlo wil zo’n enorm distributiecentrum. Maar is het nog tegen te houden?
-300 miljoen voor Amsterdams distributiecentrum, de prijs voor onze thuisbezorgmanie
-Ondanks forse kritiek groeit het aantal XXL-distributiecentra gestaag door
+        <li>
+          <a href="https://www.cobouw.nl/utiliteitsbouw/nieuws/2019/11/deze-bouwer-bouwt-geen-lelijke-grote-grijze-doos-maar-een-state-of-the-art-distributiecentrum-maat-xxxl-101278501">
+            Cobouw: Lelijke, grote, grijze doos? Nee hoor, dit wordt een state of the art distributiecentrum, maat XXXL
+          </a>
+        </li>
 
+        <li>
+          <a href="https://www.logistiek.nl/warehousing/nieuws/2020/02/2019-recordjaar-voor-xxl-distributiecentra-in-nederland-101172193">
+            2019 recordjaar voor XXL-distributiecentra in Nederland
+          </a>
+        </li>
 
-Leegstand
-CBS: Meeste oppervlakteleegstand bij kantoren en winkels
-Meer leegstand in logistiek vastgoed
-​Hoe we ervoor zorgen dat de binnenstad leegloopt
-Kaalslag retailmarkt: winkels verlaten grote stad
-Dynamis: Sprekende Cijfers Winkelmarkten
-Sociale functie van winkels
-  pretpark nederland documentaire van Michiel van Erp over de vermaakindustrie.   Bataviastad Amsterdam speelt daarin een prachtige rol.
-Infrastructuur
-ANWB: Parkeren bij winkelcentra
+        <li>
+          <a href="https://www.subcopartners.nl/pakketmarkt-groeit-uit-zn-voegen/">
+            Pakketmarkt groeit uit z’n voegen
+          </a>
+        </li>
 
-Overige/ verder lezen:
-Koopziek: Tegenlicht-serie over overmatig koopgedrag
-tps://www.volkskrant.nl/nieuws-achtergrond/voor-de-pakketbezorger-zijn-de-feestdagen-een-complete-rahtmp~b12a7a13/
-De prijs van gratis bezorging
-Waarom is Nederland zo aangeharkt en Vlaanderen zo’n rommeltje? (En meer vragen over ruimtelijke ordening)
-In de drukte van de mall springt het virus bijna zichtbaar over, maar waar moet je anders heen met je tijd en je geld?
- Retour Rumoer
-Op volle retouren - VPRO Tegenlicht - VPRO
+        <li>
+          <a href="https://www.stadsleven.nu/2020/10/10/wat-doen-we-met-de-lege-dozen/">
+            Stadsleven: Wat doen we met de lege dozen?
+          </a>
+        </li>
 
-https://www.volkskrant.nl/nieuws-achtergrond/data-tonen-ons-hamstergedrag-en-de-vele-blokjes-om~b0e45e65/
-Wehkamp boert goed in coronacrisis, en de Nederlandse consument is nog niet uitgewinkeld
-Al die dozen, al dat karton: waarom komen online bestellingen niet in herbruikbare verpakkingen?
-Online pakketjes bestellen: zo doe je het milieuvriendelijk
+        <li>
+          <a href="https://www.vn.nl/reis-pakketje-bol-com/">
+            Vandaag besteld, morgen in huis: mee op reis met een pakketje van Bol.com
+          </a>
+        </li>
 
+        <li>
+          <a href="https://www.vanbergeijk.com/portfolio/boek/binnen-bij-bol-com-undercover-bij-de-winkel-van-ons-allemaal/">
+            Jeroen van Bergeijk: Binnen bij Bol.com - undercover bij de winkel van ons allemaal
+          </a>
+        </li>
 
+        <li>
+          <a href="https://www.volkskrant.nl/nieuws-achtergrond/vijf-weken-undercover-in-het-distributiecentrum-van-bol-com~b18a9510/">
+            Vijf weken undercover in het distributiecentrum van bol.com
+          </a>
+        </li>
 
+        <li>
+          <a href="https://www.acm.nl/sites/default/files/documents/2020-05/marktstudie-last-mile-pakketbezorging.pdf">
+            Marktstudie last-mile pakketbezorging
+          </a>
+        </li>
 
-City-marketing: volkskrant wat-wil-terneuzen-bereiken-met-beeld-van-twee-neuzen-en-letters-ter~b37797b5/?utm_source=link&utm_medium=app&utm_campaign=shared%20content&utm_content=free -->
+        <li>
+          <a href="https://www.nrc.nl/nieuws/2021/10/07/niemand-in-nuenen-en-geldrop-mierlo-wil-zon-enorm-distributiecentrum-maar-valt-het-nog-te-stoppen-a4061115">
+            Niemand in Nuenen en Geldrop-Mierlo wil zo’n enorm distributiecentrum. Maar is het nog tegen te houden?
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.parool.nl/amsterdam/300-miljoen-voor-amsterdams-distributiecentrum-de-prijs-voor-onze-thuisbezorgmanie~b414c98bf/">
+            300 miljoen voor Amsterdams distributiecentrum, de prijs voor onze thuisbezorgmanie
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.volkskrant.nl/nieuws-achtergrond/ondanks-forse-kritiek-groeit-het-aantal-xxl-distributiecentra-gestaag-door~baa71826/">
+            Ondanks forse kritiek groeit het aantal XXL-distributiecentra gestaag door
+          </a>
+        </li>
+
+      </ul>
+
+      <h3>Leegstand</h3>
+
+      <ul>
+        <li>
+          <a href="https://www.cbs.nl/nl-nl/nieuws/2019/48/meeste-oppervlakteleegstand-bij-kantoren-en-winkels">
+            CBS: Meeste oppervlakteleegstand bij kantoren en winkels
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.logistiekprofs.nl/nieuws/meer-leegstand-in-logistiek-vastgoed">
+            Meer leegstand in logistiek vastgoed
+          </a>
+        </li>
+
+        <li>
+          <a href="https://retailtrends.nl/item/45972/-hoe-we-ervoor-zorgen-dat-de-binnenstad-leegloopt">
+            ​Hoe we ervoor zorgen dat de binnenstad leegloopt
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.vastgoedmarkt.nl/beleggingen/nieuws/2020/01/kaalslag-retailmarkt-winkels-verlaten-grote-stad-101151103">
+            Kaalslag retailmarkt: winkels verlaten grote stad
+          </a>
+        </li>
+
+        <li>
+          <a href="https://dynamis.nl/uploads/media/file/ed42f0d2-33ed-4bed-ad24-8aefc5b33a93/sprekende-cijfers-winkelmarkten-2019.pdf">
+          Dynamis: Sprekende Cijfers Winkelmarkten
+          </a>
+        </li>
+      </ul>
+
+      <h3>Overig</h3>
+
+      <ul>
+        <li>
+          <a href="https://www.2doc.nl/speel~NPS_1082316~pretpark-nederland-holland-doc~.html">
+            Michiel van Erp: Pretpark Nederland
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.anwb.nl/belangenbehartiging/parkeertarieven-winkelcentra">
+            ANWB: Parkeren bij winkelcentra
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.vpro.nl/programmas/tegenlicht/lees/artikelen/2020/weerstand-verleiding-consumptiemaatschappij.html">
+            VPRO Tegenlicht: Waarom jij niet de baas bent over je eigen koopgedrag
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.volkskrant.nl/nieuws-achtergrond/voor-de-pakketbezorger-zijn-de-feestdagen-een-complete-ramp~b12a7a13/">
+            Voor de pakketbezorger zijn de feestdagen 'een complete ramp'
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.volkskrant.nl/kijkverder/v/2021/de-prijs-van-gratis-bezorging~v449789/">
+          De prijs van gratis bezorging
+          </a>
+        </li>
+
+        <li>
+          <a href="https://decorrespondent.nl/12663/waarom-is-nederland-zo-aangeharkt-en-vlaanderen-zon-rommeltje-en-meer-vragen-over-ruimtelijke-ordening/761822529237-97555d33">
+            Waarom is Nederland zo aangeharkt en Vlaanderen zo’n rommeltje? (En meer vragen over ruimtelijke ordening)
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.volkskrant.nl/columns-opinie/in-de-drukte-van-de-mall-springt-het-virus-bijna-zichtbaar-over-maar-waar-moet-je-anders-heen-met-je-tijd-en-je-geld~bc294a29/">
+            In de drukte van de mall springt het virus bijna zichtbaar over, maar waar moet je anders heen met je tijd en je geld?
+          </a>
+        </li>
+
+        <li>
+          <a href="https://app.nos.nl/op3/pakjes/index.html">
+            Retour Rumoer: de groeipijn van onze bezorgingsstaat
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.volkskrant.nl/nieuws-achtergrond/data-tonen-ons-hamstergedrag-en-de-vele-blokjes-om~b0e45e65/">
+            Data tonen ons hamstergedrag en de vele blokjes om
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.volkskrant.nl/nieuws-achtergrond/wehkamp-boert-goed-in-coronacrisis-en-de-nederlandse-consument-is-nog-niet-uitgewinkeld~b572e99d/">
+            Wehkamp boert goed in coronacrisis, en de Nederlandse consument is nog niet uitgewinkeld
+          </a>
+        </li>
+
+        <li>
+          <a href="https://decorrespondent.nl/11970/al-die-dozen-al-dat-karton-waarom-komen-online-bestellingen-niet-in-herbruikbare-verpakkingen/4822326584460-50e968a0">
+            Al die dozen, al dat karton: waarom komen online bestellingen niet in herbruikbare verpakkingen?
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.volkskrant.nl/beter-leven/online-pakketjes-bestellen-zo-doe-je-het-milieuvriendelijk~bcf9bef8/">
+            Online pakketjes bestellen: zo doe je het milieuvriendelijk
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.volkskrant.nl/nieuws-achtergrond/wat-wil-terneuzen-bereiken-met-beeld-van-twee-neuzen-en-letters-ter~b37797b5/">
+            Wat wil Terneuzen bereiken met beeld van twee neuzen en letters ‘TER’?
+          </a>
+        </li>
+      </ul>
+
+      <h2>Data</h2>
+
+      <p>
+        Informatie over de data en broncode die voor dit hoofdstuk gebruikt zijn staat op <a href="https://github.com/bovenland/waar-we-winkelen">GitHub</a>.
+      </p>
 
     </section>
   </DataPage>
